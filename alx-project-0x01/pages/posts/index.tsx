24195fe99a041ed1c1505c1311a6d5f693f4c01f
HelenLight001 <helenemmanuel704@gmail.com>
2025-07-18 20:21:20 +0100

    Added some update

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,10 +1,14 @@
+import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 
 const PostsPage: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Posts</h1>
-      <PostCard />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-6 flex-grow">
+        <h1 className="text-3xl font-bold mb-4">Posts</h1>
+        <PostCard />
+      </main>
     </div>
   );
 };
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,13 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Users</h1>
-      <p>List of users will go here...</p>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="p-6 flex-grow">
+        <h1 className="text-3xl font-bold mb-4">Users</h1>
+        <p>List of users will go here...</p>
+      </main>
     </div>
   );
 };
